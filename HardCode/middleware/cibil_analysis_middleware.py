047d8f9a497538d8cc641e320b51fa811f1afa3b
atomsmasher81 <kartikgautam171@gmail.com>
2020-02-13 01:57:36 +0530

    minor

--- HardCode/middleware/cibil_analysis_middleware.py
+++ HardCode/middleware/cibil_analysis_middleware.py
@@ -3,27 +3,25 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from HardCode.scripts import bl0
 from HardCode.scripts.apicreditdata import convert_to_df
-from analysisnode.settings import BASE_DIR
-import json
 
 
 @api_view(['POST'])
 @permission_classes((IsAuthenticated,))
-def get_something(request):
+def get_cibil_analysis(request):
     try:
         user_id = int(request.data.get('user_id'))
     except:
         return Response({'status': False,'message': 'user_id parameter is required'}, 400)
     try:
-        new_user = int(request.data.get('new_user'))
+        new_user = bool(request.data.get('new_user'))
     except:
         return Response({'status': False,'message': 'new_user parameter is required'}, 400)
     try:
-        sms_json = int(request.data.get('sms_json'))
+        sms_json = request.data.get('sms_json')
     except:
         return Response({'status': False,'message': 'sms_json parameter is required'}, 400)
     try:
-        cibil_xml = int(request.data.get('cibil_xml'))
+        cibil_xml = request.data.get('cibil_xml')
     except:
         return Response({'status': False,'message': 'cibil_xml parameter is required'}, 400)
 
--- HardCode/views.py
+++ HardCode/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
 from django.views.generic.base import View
-from .middleware.cibil_analysis_middleware import get_something
-from rest_framework.response import Response
+from .middleware.cibil_analysis_middleware import get_cibil_analysis
 
-
-# @ensure_csrf_cookie
 
 class CibilAnalysis(View):
 
     def post(self, request):
         #print(request.POST)
 
-        return get_something(request,)
+        return get_cibil_analysis(request,)
