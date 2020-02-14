1937585abdcd34206bd7c2a2463e6e3d6b45d391
atomsmasher81 <kartikgautam171@gmail.com>
2020-02-15 01:58:31 +0530

    made required params mandatory

--- HardCode/middleware/cibil_analysis_middleware.py
+++ HardCode/middleware/cibil_analysis_middleware.py
@@ -8,20 +8,29 @@ from HardCode.scripts.apicreditdata import convert_to_df
 @api_view(['POST'])
 @permission_classes((IsAuthenticated,))
 def get_cibil_analysis(request):
+    print(request.data)
     try:
         user_id = int(request.data.get('user_id'))
+
     except:
         return Response({'status': False,'message': 'user_id parameter is required'}, 400)
     try:
-        new_user = bool(request.data.get('new_user'))
+        new_user = request.data.get('new_user')
+        if new_user is None:
+            raise Exception
+        new_user = bool(new_user)
     except:
         return Response({'status': False,'message': 'new_user parameter is required'}, 400)
     try:
         sms_json = request.data.get('sms_json')
+        if sms_json is None:
+            raise Exception
     except:
         return Response({'status': False,'message': 'sms_json parameter is required'}, 400)
     try:
         cibil_xml = request.data.get('cibil_xml')
+        if cibil_xml is None:
+            raise Exception
     except:
         return Response({'status': False,'message': 'cibil_xml parameter is required'}, 400)
 
