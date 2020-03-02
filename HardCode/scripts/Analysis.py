7c237ac341b4058bcea99d03820c4c1a7dafdeb4
Anandesh-Sharma <anandeshsharma@gmail.com>
2020-03-02 22:04:01 +0530

    Final changes done

--- HardCode/scripts/Analysis.py
+++ HardCode/scripts/Analysis.py
@@ -11,9 +11,42 @@ def analyse(**kwargs):
     logger = logger_1("Analysis", user_id)
     logger.info('Stariting cibil analysis')
     logger.info('user cibil check')
+
+    # if cibil found for a specific customer then run the new cibil analysis
     if cibil_df:
-        r = {'status': True, 'message': 'success', 'onhold': False, 'user_id': user_id,
-             'limit': -1, 'logic': 'BL0'}
+        Account_Status = dict()
+        Payment_Ratings = dict()
+        review = False
+        # account status
+        for acc_status in cibil_df['account_status']:
+            Account_Status[acc_status] = user_id
+        # payment ratings
+        for pay_rating in cibil_df['payment_rating']:
+            Payment_Ratings[pay_rating] = user_id
+
+        Blocked_Payment_Ratings = [3, 4, 5, 6]
+        Blocked_Status = [93, 89, 97, 32, 33, 34, 35, 37, 38, 43, 44, 45, 46, 47, 49, 50, 53, 54, 55, 56, 57, 58, 59,
+                          61,
+                          62, 63, 64, 65, 66, 67, 68, 69, 70, 72, 73, 74, 75, 76, 77, 79, 81, 85, 86, 87, 88, 94, 90]
+
+        for bpr in Blocked_Payment_Ratings:
+            if bpr in Payment_Ratings:
+                review = True
+                break
+        if not review:
+            for bs in Blocked_Status:
+                if bs in Account_Status:
+                    review = True
+                    break
+
+        if not review:
+            r = {'status': True, 'message': 'success', 'onhold': False, 'user_id': user_id,
+                 'limit': 2000, 'logic': 'BL0'}
+        else:
+            r = {'status': True, 'message': 'success', 'onhold': True, 'user_id': user_id,
+                 'limit': -1, 'logic': 'BL0'}
+
+    # else the base logic will run for the old customers having equifax score
     else:
         if int(cibil_score) >= 750:
             logger.info('returning result 3k')
