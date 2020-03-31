997698123192548d1f7e09c68b038766af758f6e
shreyagupta10 <94.guptashreya@gmail.com>
2020-03-31 13:37:39 +0530

    added secured unsecured loans in apicreditdata

--- HardCode/scripts/cibil/apicreditdata.py
+++ HardCode/scripts/cibil/apicreditdata.py
@@ -3,18 +3,21 @@ from HardCode.scripts.cibil.xmlparser import xml_parser
 
 
 def convert_to_df(file):
+    secured_loan = 0
+    unsecured_loan = 0
     d = {'written_amt_total': [], 'written_amt_principal': [], 'credit_score': [], 'payment_rating': [],
-         'payment_history': [], 'account_type': [], 'account_status': []}
+         'payment_history': [], 'account_type': [], 'account_status': [],'secured_loan':[],'unsecured_loan':[]}
     data_dict, file_found = xml_parser(file)
     if file_found:
         try:
             try:
                 acc_details = data_dict['INProfileResponse']['CAIS_Account']['CAIS_Account_DETAILS']
-                try:
-                    score = data_dict['INProfileResponse']['SCORE']
-                    credit_score = score['BureauScore']
-                except:
-                    credit_score = '0'
+                loan_type = data_dict['INProfileResponse']['CAPS']['CAPS_Application_Details']
+                # try:
+                #     score = data_dict['INProfileResponse']['SCORE']
+                #     credit_score = score['BureauScore']
+                # except:
+                #     credit_score = '0'
             except:
                 d['written_amt_total'].append('0')
                 d['written_amt_principal'].append('0')
@@ -23,10 +26,34 @@ def convert_to_df(file):
                 d['payment_rating'].append('0')
                 d['account_type'].append('0')
                 d['account_status'].append('0')
+                d['secured_loan'].append('0')
+                d['unsecured'].append('0')
                 df = pd.DataFrame(d)
                 message = "SUCCESS"
                 response = {'status': True, 'data': df, 'message': message}
                 return response
+
+            if type(loan_type) is list:
+                for acc in loan_type:
+                    if acc['Enquiry_Reason'] == '2':
+                        secured_loan += 1
+                    elif acc['Enquiry_Reason'] == '4':
+                        secured_loan += 1
+                    elif acc['Enquiry_Reason'] == '8':
+                        secured_loan += 1
+                    elif acc['Enquiry_Reason'] == '10':
+                        secured_loan += 1
+                    elif acc['Enquiry_Reason'] == '14':
+                        secured_loan += 1
+                    else:
+                        unsecured_loan += 1
+
+                secured_loan_count = secured_loan
+                unsecured_loan_count = unsecured_loan
+            else:
+                secured_loan_count = '0'
+                unsecured_loan_count = '0'
+
 
             if type(acc_details) is list:
                 for i in range(0, len(acc_details)):
@@ -66,6 +93,8 @@ def convert_to_df(file):
                     d['payment_rating'].append(str(pay_rating))
                     d['account_type'].append(acc_type)
                     d['account_status'].append(str(account_status))
+                    d['secured_loan'].append(secured_loan_count)
+                    d['unsecured_loan'].append(unsecured_loan_count)
             else:
                 try:
                     amt_total = acc_details['Written_Off_Amt_Total']
@@ -103,6 +132,12 @@ def convert_to_df(file):
                 d['payment_rating'].append(str(pay_rating))
                 d['account_type'].append(acc_type)
                 d['account_status'].append(str(account_status))
+                d['secured_loan'].append(secured_loan_count)
+                d['unsecured_loan'].append(unsecured_loan_count)
+
+                 
+
+
             df = pd.DataFrame(d)
             message = "SUCCESS"
             response = {'status': True, 'data': df, 'message': message}
@@ -115,6 +150,8 @@ def convert_to_df(file):
             d['payment_rating'].append('0')
             d['account_type'].append('0')
             d['account_status'].append('0')
+            d['secured_loan'].append('0')
+            d['unsecured_loan'].append('0')
             df = pd.DataFrame(d)
             response = {'status': False, 'data': df, 'message': message}
         return response
@@ -126,6 +163,8 @@ def convert_to_df(file):
         d['payment_rating'].append('0')
         d['account_type'].append('0')
         d['account_status'].append('0')
+        d['secured_loan'].append('0')
+        d['unsecured_loan'].append('0')
         df = pd.DataFrame(d)
         response = {'status': False, 'data': df, 'message': 'None'}
         return response
