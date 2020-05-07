5e2861f937e25985bb6ac07ec4f86595b8be1396
Garvit <garvitminocha@credicxo.com>
2020-05-07 14:36:27 +0530

    transaction regex updated

---
+++ HardCode/scripts/classifiers/Classifier_salary.py
@@ -0,0 +1,9 @@
+from HardCode.scripts.Util import conn, convert_json, logger_1
+import warnings
+import re
+from datetime import datetime
+import pytz
+
+warnings.filterwarnings("ignore")
+
+# epfoho
