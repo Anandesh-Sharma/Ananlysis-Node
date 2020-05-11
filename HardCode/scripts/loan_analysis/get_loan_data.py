5c11df92eee565b4adc19967621319b8f81a7e07
Abhishek Omi <omiabhishek91@gmail.com>
2020-05-11 14:33:27 +0530

    remove raise exception from get_loan_data module

--- HardCode/scripts/loan_analysis/get_loan_data.py
+++ HardCode/scripts/loan_analysis/get_loan_data.py
@@ -15,21 +15,18 @@ def fetch_user_data(cust_id):
     logger = logger_1('get_customer_data', cust_id)
 
     try:
-
         client = conn()
         # connect to database
         db = client.messagecluster
         logger.info("Successfully established the connection with DataBase")
-        # db = client.messagecluster
 
         # connect to collection
         approval_data = db.loanapproval
         disbursed_data = db.disbursed
         overdue_data = db.loandueoverdue
         closed_data = db.loanclosed
-        # trans_data = db.transaction
+
         closed = closed_data.find_one({"cust_id": cust_id})
-        # trans = trans_data.find_one({"cust_id": cust_id})
         disbursed = disbursed_data.find_one({"cust_id": cust_id})
         approval = approval_data.find_one({"cust_id": cust_id})
         overdue = overdue_data.find_one({"cust_id": cust_id})
@@ -40,11 +37,6 @@ def fetch_user_data(cust_id):
             logger.info("Found loan closed data")
         else:
             logger.info("loan closed data not found")
-
-        '''if trans != None:
-            transaction_df = pd.DataFrame(trans['sms'])
-        else:
-            raise Exception'''
 
         if len(disbursed['sms']) != 0:
             disbursed_df = pd.DataFrame(disbursed['sms'])
@@ -60,23 +52,13 @@ def fetch_user_data(cust_id):
         else:
             logger.info("loan overdue data not found")
 
-        if len(approval['sms']) != 0:
-            approval_df = pd.DataFrame(approval['sms'])
-            loan_data = loan_data.append(approval_df)
-            logger.info("Found loan approval data")
-        else:
-            logger.info("loan approval data not found")
-
         loan_data.sort_values(by=["timestamp"])
-        # transaction_df.sort_values(by=["timestamp"])
 
         loan_data = loan_data.reset_index(drop=True)
-        # transaction_df = transaction_df.reset_index(drop=True)
         script_status = {'status': True, "result": loan_data}
         client.close()
-    except Exception as e:
-        # script_Status['data_fetch'] = -1
-        logger.critical(e)
+    except:
+        logger.info('unable to fetch data')
         script_status = {'status': False, 'message': 'unable to fetch data'}
         client.close()
     finally:
