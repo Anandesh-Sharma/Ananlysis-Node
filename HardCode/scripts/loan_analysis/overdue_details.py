9aded5012167f7ad5578b55665094da86f3515bf
abhishek omi <omiabhishek91@gmail.com>
2020-06-05 21:08:18 +0530

    minor change in overdue details

--- HardCode/scripts/loan_analysis/overdue_details.py
+++ HardCode/scripts/loan_analysis/overdue_details.py
@@ -8,7 +8,6 @@ import pytz
 
 def get_overdue_details(cust_id):
     overdue_days_list = []
-    script_status = {}
     overdue_ratio = 0
     total_loans = 0
     report = {}
@@ -26,21 +25,15 @@ def get_overdue_details(cust_id):
                     start_date = datetime.strptime("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
                     if disbursed_date > start_date:
                         if data[i][j]['overdue_days'] != -1:
-                            #overdue_days_list["overdue_days"].append(data[i][j]['overdue_days'])
-                            #overdue_days_list["date"].append(str(data[i][j]['disbursed_date']))
                             overdue_days_list.append(data[i][j]['overdue_days'])
                             total_loans += 1
-                elif data[i][j]['due_message_date'] != -1:
+                elif data[i][j]['due_date'] != -1:
                     due_date = datetime.strptime(str(data[i][j]['due_message_date']), '%Y-%m-%d %H:%M:%S')
                     start_date = datetime.strptime("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
                     if due_date > start_date:
                         if data[i][j]['overdue_days'] != -1:
-                            #overdue_days_list['overdue_days'].append(data[i][j]['overdue_days'])
-                            #overdue_days_list["date"].append(str(data[i][j]['disbursed_date']))
                             overdue_days_list.append(data[i][j]['overdue_days'])
                             total_loans += 1
-                else:
-                    pass
         if total_loans != 0:
             overdue_ratio = np.round(len(overdue_days_list)/total_loans, 4)
         else:
